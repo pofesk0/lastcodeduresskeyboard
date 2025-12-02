bf6bd3e1825516dbdd146a64633cf358b6417c89
pofesk0 <pofesk0@gmail.com>
2025-12-02 10:11:49 +0500

    Update EnableReceiver.java

--- app/src/main/java/duress/keyboard/EnableReceiver.java
+++ app/src/main/java/duress/keyboard/EnableReceiver.java
@@ -5,18 +5,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ComponentName;
 import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Looper;
 
 public class EnableReceiver extends BroadcastReceiver {
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
 
-        PackageManager pm = context.getPackageManager();
-        ComponentName cn = new ComponentName(context, InputActivity.class);
+        Handler handler = new Handler(Looper.getMainLooper());
 
-        pm.setComponentEnabledSetting(
-			cn,
-			PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-			PackageManager.DONT_KILL_APP
-        );
+        handler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					PackageManager pm = context.getPackageManager();
+					ComponentName cn = new ComponentName(context, InputActivity.class);
+
+					pm.setComponentEnabledSetting(
+						cn,
+						PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+						PackageManager.DONT_KILL_APP
+					);
+				}
+			}, 350);
     }
 }
