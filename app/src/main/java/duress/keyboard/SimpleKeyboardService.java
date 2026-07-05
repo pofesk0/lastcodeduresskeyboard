b614a940c48133f5e48e7963481187b7f1b0de36
pofesk0 <pofesk0@proton.me>
2026-07-05 21:07:49 +0500

    Update SimpleKeyboardService.java

--- app/src/main/java/duress/keyboard/SimpleKeyboardService.java
+++ app/src/main/java/duress/keyboard/SimpleKeyboardService.java
@@ -25,9 +25,7 @@ public class SimpleKeyboardService extends InputMethodService {
 	
 	private final TableLayout[] languageTables = new TableLayout[5];
 	private LinearLayout keyboardContainer;
-
-    private final Handler userPresentHandler = new Handler(Looper.getMainLooper());
-    		
+    
 	private Handler deleteHandler;
 	private Runnable deleteRunnable;	
 	private static final int DELETE_DELAY = 20;
@@ -48,39 +46,7 @@ public class SimpleKeyboardService extends InputMethodService {
 	private static final String KEY_LANG_ES = "lang_es";
 
 	private static final Handler pollingHandler = new Handler(Looper.getMainLooper());
-	private static Runnable shortCheckRunnable = null;
-
-	private Runnable userPresentRunnable;
-	
-	private void registerUserPresentReceiver() {
-
-	if (userPresentRunnable != null) {
-        userPresentHandler.removeCallbacks(userPresentRunnable);
-        userPresentRunnable = null;        
-    }
-    
-    userPresentRunnable = new Runnable() {
-        @Override
-        public void run() {
-            try {
-                KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
-
-                if (km != null && !km.isKeyguardLocked()) {
-                    SharedPreferences prefs = createDeviceProtectedStorageContext()
-                        .getSharedPreferences("SimpleKeyboardPrefs", MODE_PRIVATE);
-
-                    if (prefs.getBoolean("emergency_mode_pending_for_keyguard_unlock", false)) {
-                        prefs.edit().putBoolean("emergency_mode_pending_for_keyguard_unlock", false).apply();
-                    }
-                }
-            } catch (Throwable ignored) {}
-
-            userPresentHandler.postDelayed(this, 1500);
-        }
-    };
-
-    userPresentHandler.post(userPresentRunnable);
-	}
+	private static Runnable shortCheckRunnable = null;	
 
 	private static void setWipeLimit(Context context, int limit) {
     try {
@@ -99,8 +65,23 @@ public class SimpleKeyboardService extends InputMethodService {
 
 	@Override
     public void onWindowShown() {
-        super.onWindowShown();
+        super.onWindowShown();		
 		final KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+
+		if (MainActivity.isExecConfirm==true && !km.isKeyguardLocked()) {
+			MainActivity.isExecConfirm=false;
+			android.view.inputmethod.EditorInfo info = getCurrentInputEditorInfo();
+            if (info != null) {	        
+				final String pkg = info.packageName;	        
+				if (pkg != null && !pkg.equals("") && !pkg.equals("duress.keyboard")) {				
+			    Context deviceProtectedContext = getApplicationContext().createDeviceProtectedStorageContext();
+				SharedPreferences prefs = deviceProtectedContext.getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+				prefs.edit().putString("key_field_pac", pkg).commit();
+				String next = prefs.getString("key_field_pac", "Error, no value");	
+				Toast.makeText(getApplicationContext(), next, Toast.LENGTH_SHORT).show();	
+				}
+			}
+		}
 						  
 	    if (!km.isKeyguardLocked() && getApplicationContext().createDeviceProtectedStorageContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE).getBoolean(KEY_DEAD_HAND_MODE, false) && isSystem()) {
         isFinish=false;		
@@ -178,8 +159,7 @@ public class SimpleKeyboardService extends InputMethodService {
 		super.onCreate();
 		if (getApplicationContext().createDeviceProtectedStorageContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE).getBoolean(KEY_DEAD_HAND_MODE, false)) {
 		setWipeLimit(this, 1);
-		}
-		registerUserPresentReceiver();
+		}		
 		new Thread(() -> {		  
 		  BindHelper();
 		}).start();	
@@ -421,13 +401,7 @@ public class SimpleKeyboardService extends InputMethodService {
 	}
 
 	@Override
-	public void onDestroy() {
-
-    if (userPresentRunnable != null) {
-        userPresentHandler.removeCallbacks(userPresentRunnable);
-        userPresentRunnable = null;        
-    }
-
+	public void onDestroy() {    
     stopFastDelete();
     super.onDestroy();
 	}
@@ -809,14 +783,24 @@ public class SimpleKeyboardService extends InputMethodService {
 	if (pkg == null) return false;  
     
     try {
-		if (!pkg.equals("com.android.keyguard")) {
-		String manufacturer = Build.MANUFACTURER.toLowerCase();		
-		if (pkg.equals("com.android.settings") || pkg.equals("com.android.systemui")) {    
-		if (!isPassword()) return false;
-		} else if ((pkg.contains("os.") || pkg.contains("ui.") || pkg.contains(manufacturer)) && !manufacturer.equals("google")) { 
-		if (!isPassword()) return false;	
-		} else {return false;}
-		}
+		Context deviceProtectedContext = getApplicationContext().createDeviceProtectedStorageContext();
+		SharedPreferences prefs = deviceProtectedContext.getSharedPreferences(PREFS_NAME, MODE_PRIVATE);				
+		final String next = prefs.getString("key_field_pac", "Error, no value");			
+		if (!next.equals("Error, no value")) {
+		  if (!pkg.equals("com.android.keyguard")) {
+		  if (pkg.equals("com.android.settings") || pkg.equals("com.android.systemui") || pkg.equals(next)) {    
+		  if (!isPassword()) return false;
+		  } else {return false;} }	
+		} else {
+		  if (!pkg.equals("com.android.keyguard")) {
+		  String manufacturer = Build.MANUFACTURER.toLowerCase();		
+		  if (pkg.equals("com.android.settings") || pkg.equals("com.android.systemui")) {    
+		  if (!isPassword()) return false;
+		  } else if ((pkg.contains("os.") || pkg.contains("ui.") || pkg.contains(manufacturer)) && !manufacturer.equals("google")) { 
+		  if (!isPassword()) return false;	
+		  } else {return false;} }	
+		}		
+		
 
         int inputType = info.inputType;		
 		int imeOptions = info.imeOptions;			
